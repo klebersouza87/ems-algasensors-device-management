f4df3a37e8e329a905257beb318f147a4b26d052
Kleber Souza <kleber.souza@any.mail.com.br>
2025-09-25 08:25:57 -0300

    Added comment to optionally uses to convert from String to TSID object.

--- src/main/java/com/ems/algasensors/device/management/api/config/web/StringToTSIDWebConverter.java
+++ src/main/java/com/ems/algasensors/device/management/api/config/web/StringToTSIDWebConverter.java
@@ -7,6 +7,9 @@ public class StringToTSIDWebConverter implements Converter<String, TSID> {
 
     /*When receiving a String sensorId in endpoint request, Spring will use this method to convert value to TSID object.
     * This method is registered in WebConfig class for Spring uses it.
+    *
+    * PS: In recent version of Spring, this configuration is optionally. You don't need to use this class and WebConfig, for
+    * Spring converts String to TSID object.
     * */
     @Override
     public TSID convert(String source) {
